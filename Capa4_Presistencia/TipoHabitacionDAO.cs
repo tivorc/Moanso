0799861289ba8bec9c77fb11d75ed5fe102d728f
JeanPierre <pierleo1997@hotmail.com>
2016-11-26 20:13:39 -0500

    Se modifico tipo de habitacion

---
+++ Capa4_Presistencia/TipoHabitacionDAO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Capa3_Dominio;
+namespace Capa4_Presistencia
+{
+    class TipoHabitacionDAO
+    {
+        private GestorDAOSQL gestorDAOSQL;
+        public TipoHabitacionDAO(GestorDAOSQL gestorDAOSQL)
+        {
+            this.gestorDAOSQL = gestorDAOSQL;
+        }
+        public int guardar(TipoHabitacion tipohabitacion)
+        {
+            int registro_afectados;
+            string sentenciaSQL = "inset into TipoHabitacion(nombre, descripcion, precio) values(@nombre, @descripcion, @precio)";
+            try
+            {
+                SqlCommand comando = gestorDAOSQL.obtenerComandoSQL(sentenciaSQL);
+                comando.Parameters.AddWithValue("@nombre",tipohabitacion.Nombre);
+                comando.Parameters.AddWithValue("@descripcion", tipohabitacion.Descripcion);
+                comando.Parameters.AddWithValue("@precio", tipohabitacion.Precio);
+                registro_afectados = comando.ExecuteNonQuery();
+                return registro_afectados;
+
+
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+    }
+}
