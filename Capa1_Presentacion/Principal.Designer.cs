562649a41ca40c38d5ac6e5a513fac55cc9ec0ea
Jimmy <jiimmi.17@hotmail.com>
2016-11-26 20:57:18 -0500

    FORMPRINCIPAL

---
+++ Capa1_Presentacion/Principal.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Capa1_Presentacion
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Principal));
+            this.btnReporte = new System.Windows.Forms.Button();
+            this.btnHabitacion = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnReporte
+            // 
+            this.btnReporte.Image = ((System.Drawing.Image)(resources.GetObject("btnReporte.Image")));
+            this.btnReporte.Location = new System.Drawing.Point(35, 317);
+            this.btnReporte.Name = "btnReporte";
+            this.btnReporte.Size = new System.Drawing.Size(81, 75);
+            this.btnReporte.TabIndex = 0;
+            this.btnReporte.UseVisualStyleBackColor = true;
+            // 
+            // btnHabitacion
+            // 
+            this.btnHabitacion.Image = ((System.Drawing.Image)(resources.GetObject("btnHabitacion.Image")));
+            this.btnHabitacion.Location = new System.Drawing.Point(31, 51);
+            this.btnHabitacion.Name = "btnHabitacion";
+            this.btnHabitacion.Size = new System.Drawing.Size(85, 85);
+            this.btnHabitacion.TabIndex = 1;
+            this.btnHabitacion.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(31, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 108);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 432);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnHabitacion);
+            this.Controls.Add(this.btnReporte);
+            this.Name = "Principal";
+            this.Text = "Principal";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReporte;
+        private System.Windows.Forms.Button btnHabitacion;
+        private System.Windows.Forms.Button button1;
+    }
+}
