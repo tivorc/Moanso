26c4a212686de43a2eb014e9f21a9eff2bb453f7
victor <victoredin94@hotmail.com>
2016-11-27 17:27:39 -0500

    XDDDDDDDD

--- Capa3_Dominio/Cliente.cs
+++ Capa3_Dominio/Cliente.cs
@@ -15,7 +15,7 @@ namespace Capa3_Dominio
         public string Apellido_materno { get; set; }
         public string Direccion { get; set; }
         public string Telefono { get; set; }
-        public char Tipo_cliente { get; set; }
+        public string Tipo_cliente { get; set; }
         public string Ruc { get; set; }
         public string Razon_social { get; set; }
         public TipoDocumento Tipo_documento { get; set; }
